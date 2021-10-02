0efb7129a0ca0d75c3c6ee5ec2d906feb2d8bfbc
Diggaj Upadhyay <dcozupadhyay@gmail.com>
2021-10-02 19:33:56 +0800

    new files

---
+++ Border.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Border here.
+ * 
+ * @author Diggaj Upadhyay 
+ * @version 20.09.2021
+ */
+public class Border extends Actor
+{
+    public Border()
+    {
+        GreenfootImage image=getImage();
+        image.scale(15,15);
+        setImage(image);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ foreach.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class foreach here.
+ * 
+ * @author Diggaj Upadhyay 
+ * @version 20.09.2021
+ */
+public class foreach extends Actor
+{
+    /**
+     * Act - do whatever the foreach wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //List<Actor> actors = new ArrayList<Actor>();
+        
+    }    
+}
